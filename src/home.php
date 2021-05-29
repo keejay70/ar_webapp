862bfe07c0834fdc5e488e22d4f27fd7c933a118
keejay70 <setenta.kent@gmail.com>
2021-05-29 22:52:42 +0800

    table

--- src/home.php
+++ src/home.php
@@ -11,6 +11,10 @@
         nav {
             margin-bottom: 40px;
         }
+        .table {
+            width: 80%;
+            margin: 30px auto;
+        }
     </style>
     <body>
     <!-- As a heading -->
@@ -68,16 +72,16 @@
                         <table class="table">
                         <thead>
                             <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                            <th >Action</th>
+                            <th>#</th>
+                            <th>First</th>
+                            <th>Last</th>
+                            <th>Handle</th>
+                            <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                            <th scope="row">1</th>
+                            <th>1</th>
                             <td>Mark</td>
                             <td>Otto</td>
                             <td>@mdo</td>
